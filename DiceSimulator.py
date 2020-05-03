185bc374dbac4c501adf5279bace30afb9931fd2
Kartik Pawar <33437982+KartikPawar24@users.noreply.github.com>
2020-05-03 23:30:54 +0530

    Add files via upload

---
+++ DiceSimulator.py
@@ -0,0 +1,48 @@
+from random import randint
+
+x = 'y'
+while x == 'y':
+    number = randint(1, 6)
+
+    if number == 1:
+        print("-----------")
+        print("|         |")
+        print("|    O    |")
+        print("|         |")
+        print("-----------")
+    if number == 2:
+        print("-----------")
+        print("|         |")
+        print("| O     O |")
+        print("|         |")
+        print("-----------")
+    if number == 3:
+        print("-----------")
+        print("|    O    |")
+        print("|    O    |")
+        print("|    O    |")
+        print("-----------")
+    if number == 4:
+        print("-----------")
+        print("| O     O |")
+        print("|         |")
+        print("| O     O |")
+        print("-----------")
+    if number == 5:
+        print("-----------")
+        print("| O     O |")
+        print("|    O    |")
+        print("| O     O |")
+        print("-----------")
+    if number == 6:
+        print("-----------")
+        print("| O     O |")
+        print("| O     O |")
+        print("| O     O |")
+        print("-----------")
+
+    x = input("Press y to continue: ")
+
+    if x != 'y':
+        print("Thank you for playing with me")
+        break
